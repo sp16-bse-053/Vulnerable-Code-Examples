d5cdeb49712930a22a7f4dd78058749438deda93
Arslan Ahmad Khan <52409329+sp16-bse-053@users.noreply.github.com>
2022-05-25 17:33:50 +0500

    Add files via upload

---
+++ src/array_sum/Array_Sum.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array_sum;
+
+/**
+ *
+ * @author dell
+ */import java.util.Scanner;
+public class Array_Sum {
+
+    
+    
+    
+    public static void main(String[] args) {
+       Scanner inp = new Scanner (System.in);
+       int array[];
+       int sum=0;
+       array = new int [10];
+       System.out.println("Please Enter TEN Integers to find Sum and their Average");
+       
+       for (int i=0; i<10; i++){
+           array[i] = inp.nextInt();
+           sum+=array[i];
+       }
+        double avg = (double) sum/10;
+        System.out.println("Sum is  =  "+sum);
+          System.out.println("Average is  =  "+avg);
+        
+        
+        
+    }
+    
+}
